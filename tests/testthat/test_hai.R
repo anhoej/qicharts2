56702aef19928442aad6a967a24477867f290015
anhoej <jacob@anhoej.net>
2017-08-05 11:51:07 +0200

    Revised tests.

--- tests/testthat/test_hai.R
+++ tests/testthat/test_hai.R
@@ -4,47 +4,51 @@ context('Hospital Infections')
 # Single plot of hospital infections ----
 test_that('Single plot of hospital infections', {
   expect_equal_to_reference(
-    qic(month, n, days,
-        data = hospital_infections,
-        chart = 'u')$data
-    ,
-    'hai_01.rds'
+    summary(
+      qic(month, n, days,
+          data = hospital_infections,
+          chart = 'u')
+    ),
+    'hai_1.rds'
   )
 })
 
 # Single facted plot of hospital infections ----
 test_that('Single facted plot of hospital infections', {
   expect_equal_to_reference(
-    qic(month, n, days,
-        data = hospital_infections,
-        facets = ~ infection,
-        chart = 'u')$data
-    ,
-    'hai_02.rds'
+    summary(
+      qic(month, n, days,
+          data = hospital_infections,
+          facets = ~ infection,
+          chart = 'u')
+    ),
+    'hai_2.rds'
   )
 })
 
 # Double faceted plot of hospital infections ----
 test_that('Double faceted plot of hospital infections', {
   expect_equal_to_reference(
-    qic(month, n, days,
-        data = hospital_infections,
-        facets = hospital ~ infection,
-        chart = 'u')$data
-    ,
-    'hai_03.rds'
+    summary(
+      qic(month, n, days,
+          data = hospital_infections,
+          facets = hospital ~ infection,
+          chart = 'u')
+    ),
+    'hai_3.rds'
   )
 })
 
 # Funnel plot of hospital infections ----
 test_that('Funnel plot of hospital infections', {
   expect_equal_to_reference(
-    qic(reorder(hospital, days), n, days,
-        data = subset(hospital_infections, month >= '2016-07-01'),
-        chart = 'u',
-        facets = ~ infection,
-        multiply = 10000)$data
-    ,
-    'hai_04.rds'
+    summary(
+      qic(reorder(hospital, days), n, days,
+          data = subset(hospital_infections, month >= '2016-07-01'),
+          chart = 'u',
+          facets = ~ infection,
+          multiply = 10000)
+    ),
+    'hai_4.rds'
   )
 })
