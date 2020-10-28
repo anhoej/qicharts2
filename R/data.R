b798d37b14ce6ee8b127fe17c529bb967b50ac33
anhoej <jacob@anhoej.net>
2020-10-28 06:59:55 +0100

    Preparing v. 0.7.0.

--- R/data.R
+++ R/data.R
@@ -1,16 +1,17 @@
 #' Hospital acquired infections
-#' 
-#' A dataset containing the number of hospital acquired bacteremia, Clostridium 
-#' difficile infections, and urinary tract infections in six hospitals in the 
+#'
+#' A dataset containing the number of hospital acquired bacteremia, Clostridium
+#' difficile infections, and urinary tract infections in six hospitals in the
 #' Capital Region of Denmark 2015-2016.
-#' 
-#' @format A data frame with 432 rows and 5 variables: \itemize{ \item 
-#'  {hospital} Abbreviated hospital name. \item{infection} Type 
-#'   of infection. BAC: Bacteremia, CDI: Clostridium difficile infection. UTI:
-#'   Urinary tract infection. \item{month} First day of month. \item
-#'  {n} Number of cases. \item{days} Number of risk days. A risk
-#'   day is a patient day without infection. }
-#' @source \url{http://www.haiba.dk/} (Capital Region of Denmark).
+#'
+#' @format A data frame with 432 rows and 5 variables: \itemize{ \item
+#'   {hospital} Abbreviated hospital name. \item{infection} Type of infection.
+#'   BAC: Bacteremia, CDI: Clostridium difficile infection. UTI: Urinary tract
+#'   infection. \item{month} First day of month. \item {n} Number of cases.
+#'   \item{days} Number of risk days. A risk day is a patient day without
+#'   infection. }
+#' @source \url{https://www.esundhed.dk/Emner/Patienter-og-sygehuse/HAIBA/}
+#'   (Capital Region of Denmark).
 "hospital_infections"
 
 #' Coronary artery bypass graft operations
@@ -27,16 +28,17 @@
 "cabg"
 
 #' Clostridium difficile infections
-#' 
-#' A dataset with data on hospital acquired Clostridium difficile infections 
+#'
+#' A dataset with data on hospital acquired Clostridium difficile infections
 #' (CDI) before and after an intervention to reduce the risk of CDI.
-#' 
-#' @format A data frame with 36 rows and 5 variables: \itemize{ \item 
-#'  {month} Month of observation. \item{n} Number of hospital 
-#'   acquired CDI. \item{days} Number of risk days. A risk day is a 
-#'   patient day without CDI. \item{period} Factor indicating the period
-#'   'pre' or 'post' intervention. \item{notes} Annotations. }
-#' @source \url{http://www.haiba.dk/} (Amager Hvidovre Hospital).
+#'
+#' @format A data frame with 36 rows and 5 variables: \itemize{ \item {month}
+#'   Month of observation. \item{n} Number of hospital acquired CDI. \item{days}
+#'   Number of risk days. A risk day is a patient day without CDI. \item{period}
+#'   Factor indicating the period 'pre' or 'post' intervention. \item{notes}
+#'   Annotations. }
+#' @source \url{https://www.esundhed.dk/Emner/Patienter-og-sygehuse/HAIBA/}
+#'   (Amager Hvidovre Hospital).
 "cdi"
 
 #' NHS accidents
@@ -53,16 +55,18 @@
 "nhs_accidents"
 
 #' Patient harm indentified using the Global Trigger Tool
-#' 
+#'
 #' A dataset with data on adverse events during hospitalisation found by the
 #' Global Trigger Tool.
-#' 
-#' @format A data frame with 340 rows and 11 variables: \itemize{ \item 
-#'   admission_id Admission ID. \item admission_dte Date of admission. \item 
+#'
+#' @format A data frame with 340 rows and 11 variables: \itemize{ \item
+#'   admission_id Admission ID. \item admission_dte Date of admission. \item
 #'   discharge_dte Date of discharge. \item month Month of discharge. \item days
 #'   Duration of hospital stay in days. \item harms Number of adverse events.
 #'   \item {E-I} Type of adverse event by severity category. E-F: Temporary
 #'   harm; G-H: Permanent harm; I: Fatal harm. }
 #' @source Omitted for privacy concerns.
-#' @references \url{http://www.ihi.org/resources/Pages/Tools/IHIGlobalTriggerToolforMeasuringAEs.aspx}
+#' @references
+#'   \url{http://www.ihi.org/resources/Pages/Tools/IHIGlobalTriggerToolforMeasuringAEs.aspx}
+#'   
 "gtt"
